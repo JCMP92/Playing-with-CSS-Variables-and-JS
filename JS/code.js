2853a6bea4effa2bf1274c237f8218c223c48963
JCMP92 <arq_juanmp@outlook.com>
2022-01-10 16:27:57 -0600

    Proyect finished

--- JS/code.js
+++ JS/code.js
@@ -1,7 +1,10 @@
 const inputs = document.querySelectorAll('.controls input');
 
 function handleUpdate() {
-    console.log(this.value);    
+    const suffix = this.dataset.sizing || ''; //The '' stands for 'nothing' because we also have a color value in one of the inputs, and not just 'px'
+    //console.log(this.dataset); The dataset will console.log an object with all the data that´s inside of the 'data-' that we had declared. In this case, will be the 'px' of 'data-sizing'
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
 
 inputs.forEach(inputElement => inputElement.addEventListener('change', handleUpdate));
+inputs.forEach(inputElement => inputElement.addEventListener('mousemove', handleUpdate));
