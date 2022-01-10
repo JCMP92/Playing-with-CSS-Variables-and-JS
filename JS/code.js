12bfd66901a9b2f1e35c20fc428b047a219c528e
JCMP92 <arq_juanmp@outlook.com>
2022-01-10 14:29:56 -0600

    JS modification

--- JS/code.js
+++ JS/code.js
@@ -1,1 +1,7 @@
 const inputs = document.querySelectorAll('.controls input');
+
+function handleUpdate() {
+    console.log(this.value);    
+}
+
+inputs.forEach(inputElement => inputElement.addEventListener('change', handleUpdate));
